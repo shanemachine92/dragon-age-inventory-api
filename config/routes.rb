36a577f31c876094ec5f48cd1e568a404c3dc615
Shane Zackery <shane@entelo.com>
2017-10-18 17:42:47 -0700

    add dummy inventories controller v2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :inventories do
-    resources :items
+
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :inventories, only: :index
+  end
+
+  scope module: :v1, contraints: ApiVersion.new('v1', true) do
+    resources :inventories do
+      resources :items
+    end
   end
 
   post 'signup', to: 'users#create'
